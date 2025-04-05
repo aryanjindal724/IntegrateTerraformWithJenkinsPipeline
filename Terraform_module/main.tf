429b5afd358ed985f7ac2b94b7604a0495eea28c
Aryan Jindal <aryanjindal455@gmail.com>
2025-04-05 15:28:12 +0530

    Update main.tf

--- Terraform_module/main.tf
+++ Terraform_module/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_service_plan" "this" {
 }
 
 resource "azurerm_linux_web_app" "this" {
-  name                = "webapijenkinsaryan01"
+  name                = "webapiIntegratedAryan01"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   service_plan_id     = azurerm_service_plan.this.id  # Updated attribute
